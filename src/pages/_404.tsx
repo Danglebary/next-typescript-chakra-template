e80f28a3537fbcf74a43b2c9b52e6120f1a4c36b
Danglebary <halfblowncontact@gmail.com>
2022-02-23 21:08:53 -0600

    added 404 page

---
+++ src/pages/_404.tsx
@@ -0,0 +1,15 @@
+import { Heading } from '@chakra-ui/react'
+import Page from '../components/layout/page'
+
+const Home = () => {
+    return (
+        <Page
+            title="Next-Typescript-Chakra: 404"
+            description="404 not found page of application"
+        >
+            <Heading textAlign="center">404: not found</Heading>
+        </Page>
+    )
+}
+
+export default Home
